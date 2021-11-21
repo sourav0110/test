a1afd42d567bbf45cb153d0d7e1a4956bb5549f7
sourav0110 <souravbhattacharjee926@gmail.com>
2021-11-21 09:40:04 +0530

    test2

--- app/src/main/java/com/example/myapplication/testActivity.java
+++ app/src/main/java/com/example/myapplication/testActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class testActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class testActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        Toast.makeText(testActivity.this,"test",Toast.LENGTH_LONG).show();
     }
 }
